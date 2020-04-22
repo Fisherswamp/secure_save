d71c2b74606ac797ffa3040c3c96fd4787e890bc
Itai Fish <chessandpotatoes@gmail.com>
2020-04-22 16:40:19 -0400

    alright im goofin with bcrypt I guess??

--- src/compute.rs
+++ src/compute.rs
@@ -42,3 +42,56 @@ fn generate_salt() -> String {
     status.unwrap();
     hex::encode(salt_arr)
 }
+
+
+pub struct BlowfishState {
+    
+}
+
+fn bcrypt(cost: u8, salt: [u8; 16], password: Vec<u8>) {
+    let state = eks_blowfish_setup(&cost, &salt, &password);
+    let mut cypher_text = "OrpheanBeholderScryDoubt";//This string is hardcoded into the algo apparently
+    for _ in 0..64 {
+        cypher_text = encrypt_ecb(state, cypher_text);
+    }
+    return cypher_text;
+}
+
+fn eks_blowfish_setup(cost: &u8, salt: &[u8; 16], password: &Vec<u8>) {//expensive key setup
+    let mut state = intial_state();
+    state = expand_key(state, salt, password);
+    let num_iterations = u32::pow(2, (*cost).into());
+    for _ in 0..num_iterations {
+        state = expand_key(state, 0, password);
+        state = expand_key(state, 0, salt);
+    }
+    return state;
+}
+
+fn expand_key(state: BlowfishState, salt: &[u8; 16], password: &Vec<u8>) -> BlowfishState{
+    let length = password.len();
+    //convert password from u8 to u32
+    let mut p_array: Vec<u32> = Vec::new();
+    let mut num_size = 0;
+    let mut building_num: u32 = 0;
+    for password_byte_index in 0..password.len() {
+        if num_size >= 4 {
+            p_array.push(building_num);
+            building_num = 0;
+            num_size = 0;
+        }
+        let adding_chunk: u32 = (*password)[password_byte_index].into();
+        building_num = (building_num << 4) + adding_chunk;
+        num_size = num_size + 1;
+    }
+    //
+    for n in 1..18 {
+        let index = 4*(n-1);        
+        p_array[n] = p_array[n] ^  p_array[index];
+    }
+    BlowfishState {
+
+    }
+}
+
+
